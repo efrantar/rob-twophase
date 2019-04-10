1ac1301bb4f460f79f81631463487a37fc8a5168
Elias Frantar <elias.frantar@gmail.com>
2019-04-10 13:51:05 +0200

    Pruning table now compiles

--- prun.cc
+++ prun.cc
@@ -1,68 +1,92 @@
 #include "prun.h"
 
-#include <algorithm>
+#include <bitset>
 
+#define BACKSEARCH_DEPTH 9
 #define EMPTY 0x3
 
-uint8_t *phase1_prun;
+uint64_t *fssymtwist_prun;
 
-void getFSSymTwistPrun(LargeCoord c) {
+int getFSSymTwistPrun(LargeCoord c) {
+  uint64_t tmp = fssymtwist_prun[c / 16];
+  tmp >>= (c % 16) * 2;
+  return tmp & 0x3;
 }
 
-int getFSSymTwistPrun(LargeCoord c, int depth) {
+void setFSSymTwistPrun(LargeCoord c, int depth) {
+  int shift = (c % 16) * 2;
+  fssymtwist_prun[c / 16] &= ~(0x3 << shift) | (depth % 3) << shift;
 }
-
-void 
 
 void initFSSymTwistPrun() {
-  phase1_prun = new uint8_t[N_FSSYMTWIST_COORDS];
+  fssymtwist_prun = new uint64_t[N_FSSYMTWIST_COORDS / 4];
+  std::fill(fssymtwist_prun, fssymtwist_prun + N_FSSYMTWIST_COORDS / 4, 0xffffffff);
   
   int filled = 0;
   int depth = 0;
+  std::bitset<N_FSSYMTWIST_COORDS> bits;
+  bool backsearch = false;
 
+  setFSSymTwistPrun(0, 0);
   while (filled < N_FSSYMTWIST_COORDS) {
     LargeCoord c = 0;
-    for (
-      SymCoord flipslice_sym = 0;
-      flipslice_sym < N_FLIPSLICE_SYM_COORDS;
-      flipslice_sym++
-    ) {
-      Coord flip = FS_FLIP(flipslice_raw[flipslice_sym]);
-      Coord slice = FS_SLICE(flipslice_raw[flipslice_sym]);
+    for (SymCoord fssym = 0; fssym < N_FLIPSLICE_SYM_COORDS; fssym++) {
+      Coord flip = FS_FLIP(flipslice_raw[fssym]);
+      Coord slice = FS_SLICE(flipslice_raw[fssym]);
 
-      for (Coord twist = 0; twist < N_TWIST_COORDS; twist++, c++) { 
-        if (bitset[c] || getFSSymTwistPrun(c) != EMPTY)
-          continue;
+      for (Coord twist = 0; twist < N_TWIST_COORDS; ) {
+        if (!backsearch) {
+          if (c % 16 == 0 && fssymtwist_prun[c] == 0xffffffff) {
+            twist += 16;
+            c += 16;
+            continue;
+          }
+          if (bits.test(c) || getFSSymTwistPrun(c) == EMPTY)
+            continue;
+          bits.set(c);
+        } else if (getFSSymTwistPrun(c) != EMPTY)
+         continue;
 
         for (int m = 0; m < N_MOVES; m++) {
           Coord flip1 = flip_move[flip][m];
           Coord slice1 = sliceMove(slice1, m);
           Coord twist1 = twist_move[twist][m];
-          SymCoord flipslice1_sym = flipslice_sym[FLIPSLICE(flip1, slice1)];
+          SymCoord fssym1 = flipslice_sym[FLIPSLICE(flip1, slice1)];
 
-          LargeCoord c1 = FSSYMTWIST(flipslice1_sym, twist1);
-          if (getFSSymTwistPrun(c1) == EMPTY) {
-            setFSSymTwistPrun(c1, depth1 + 1);
-            filled++;
+          LargeCoord c1 = FSSYMTWIST(fssym1, twist1);
+          if (backsearch) {
+            if (getFSSymTwistPrun(c1) != EMPTY)
+              continue;
+            c1 = c;
+            fssym1 = fssym;
+          } else if (getFSSymTwistPrun(c1) == EMPTY)
+            continue;
+     
+          setFSSymTwistPrun(c1, depth + 1);
+          filled++;
             
-            SymSet symset = flipslice_symset[flipslice1_sym];
-            if (symset > 1) {
-              Sym s = 1;
-              while (smyset > 0) {
-                symset >>= 1;
-                if (symset & 1) {
-                  LargeCoord c2 = FSSYMTWIST(flipslice1_sym, conj_twist[twist1][s]);
-                  if (getFSSymTwistPrun(c2) == EMPTY) {
-                    setFSSymTWistPrun(c2, depth + 1);
-                    filled++;
-                  }
-                }
-                s++;
+          SymSet symset = flipslice_symset[fssym1] >> 1;
+          for (Sym s = 1; symset > 0; symset >>= 1, s++) {
+            if (symset & 1) {
+              LargeCoord c2 = FSSYMTWIST(fssym1, conj_twist[twist1][s]);
+              if (getFSSymTwistPrun(c2) == EMPTY) {
+                setFSSymTwistPrun(c2, depth + 1);
+                filled++;
               }
             }
           }
+
+          if (backsearch)
+            break;
         }
+
+        twist++;
+        c++;
       }
+
+      depth++;
+      if (depth == BACKSEARCH_DEPTH)
+        backsearch = true;
     }
   } 
 }
--- prun.h
+++ prun.h
@@ -3,11 +3,14 @@
 
 #include <stdint.h>
 
+#include "coord.h"
+#include "sym.h"
+
 #define N_FSSYMTWIST_COORDS (N_FLIPSLICE_SYM_COORDS * N_TWIST_COORDS)
 
 #define FSSYMTWIST(fs_sym, twist) (fs_sym * N_TWIST_COORDS + twist)
 
-extern uint8_t *fssymtwist_prun;
+extern uint64_t *fssymtwist_prun;
 
 void getDepthFSSymTwist(LargeCoord flipslicetwist);
 
